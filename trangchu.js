be03d17530b86904bb8eacfe1e14b4924a6126ca
auquocthien <80771990+auquocthien@users.noreply.github.com>
2022-04-24 08:48:01 +0700

    first commit

--- trangchu.js
+++ trangchu.js
@@ -1,8 +1,8 @@
 let slideIndex = 1;
 showSides(slideIndex);
 
-function plusSlide(n){
-    slideIndex(slideIndex += n);
+function plushSlide(n){
+    showSides(slideIndex += n);
 }
 
 function currentSlide(n){
@@ -11,7 +11,7 @@ function currentSlide(n){
 
 function showSides(n){
     let i;
-    let slides = document.getElementsByClassName("mySlides");
+    let slides = document.getElementsByClassName("slide");
     let dost = document.getElementsByClassName("dot");
     if(n > slides.length){
         slideIndex = 1;
@@ -20,6 +20,9 @@ function showSides(n){
         slideIndex = slides.length
     }
     for(i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    for(i = 0; i < dost.length; i++){
         dost[i].className = dost[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
